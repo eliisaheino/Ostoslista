ccec99339d3a083c74b011e144861460bf0870c7
Your Name <eliisa.heino@myy.haaga-helia.fi>
2023-01-29 17:23:43 +0200

    valmis 29.1.2023

--- App.js
+++ App.js
@@ -1,10 +1,48 @@
+import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button,  TextInput, FlatList, SafeAreaView} from 'react-native';
+
 
 export default function App() {
+//Tilamuuttujat ja niiden alustaminen
+//syötetylle tekstille ja flatList osille
+const [text, setText] = useState ('');
+const [data, setData] = useState ([ ]);
+
+//Funktio lisää tekstin listaan
+const pressedAdd = () => {
+  setData([...data, { key: text }]);
+  setText('');
+}
+
+//Funktio tyhjentää listan
+const pressedClear = () => {
+  setData([]);
+} 
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Shopping list </Text>
+      {/*Syöttökenttä ostosten kirjoittamiseen*/}
+      {/*onChange antaa muuttujalle sisällön */}
+      <TextInput 
+          style={styles.input} 
+          onChangeText= {text=> setText(text)}
+          value={text}/>
+
+       {/*Painikkeet ja funktiokutsut */} 
+       <View style={styles.container2}>
+       <Button onPress={pressedAdd} title= "ADD" /> 
+       <Button onPress={pressedClear} title= "CLEAR" /> 
+       </View>
+
+       {/*Flatlist listan näyttämiseen */}
+       <FlatList 
+        data={data}
+        renderItem={({item}) => <Text style ={styles.item}>{item.key}</Text>}
+        keyExtractor ={(item, index) => index.toString() } />
+        
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +55,28 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  container2: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: '#fff',
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+  },
+  input : {
+    marginTop: 100,
+    width:200  , 
+    height: 30,
+    borderColor: 'gray', 
+    borderWidth: 1
+  },
+  text:{
+    fontSize: 20,    
+  },
+  item: {
+    padding : 5,
+    fontSize: 20,
+    marginTop: 1,
+  }
+  
+  
 });
